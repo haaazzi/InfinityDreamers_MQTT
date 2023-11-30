9fd33df4f3a2fd669e95fe16575b0c52cf6f51eb
SeinJ <drasein@gmail.com>
2023-11-30 17:23:14 +0900

    ADD: Images

---
+++ src/test/java/com/infinitydreamers/NodeTest.java
@@ -0,0 +1,14 @@
+package com.infinitydreamers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NodeTest {
+    
+    @Test
+    @BeforeEach
+    void setUp(){
+        
+    }
+}
